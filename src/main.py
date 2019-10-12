0bbe0531cd4e1a55d915d76fb1f193d9dc52a6c2
Colin Chan <colin.th.chan@gmail.com>
2019-10-12 16:41:26 -0400

    Add loop through input fields for html rendering

--- src/main.py
+++ src/main.py
@@ -4,20 +4,43 @@ from flask import (
     request
 )
 
+from .constants import (
+    BASIC_INFO,
+    FEDERAL_INCOME,
+    FEDERAL_INCOME_ADJUSTMENTS,
+    BUSINESS_INCOME,
+    FEDERAL_DEDUCTIONS,
+    ESTIMATED_TAX_PAYMENTS
+)
+
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def tax_form():
-    return render_template('index.html')
+    categories = [
+        BASIC_INFO,
+        FEDERAL_INCOME,
+        FEDERAL_INCOME_ADJUSTMENTS,
+        BUSINESS_INCOME,
+        FEDERAL_DEDUCTIONS,
+        ESTIMATED_TAX_PAYMENTS
+    ]
+    return render_template('index.html', categories=categories)
 
 @bp.route('/submit', methods=['POST'])
 def submit_form():
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    full_name = "{} {}".format(first_name, last_name)
+    # first_name = request.form.get("first_name")
+    # last_name = request.form.get("last_name")
+    # full_name = "{} {}".format(first_name, last_name)
 
-    return render_template('index.html', name=full_name)
+    # return render_template('index.html', name=full_name)
+    return
+
 
 @bp.route('/get', methods=['GET'])
 def get_form_data():
+    return
+
+@bp.route('/download', methods=['GET'])
+def download_pdf():
     return
